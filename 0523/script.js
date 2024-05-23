cffe5a6f856a63079088e0f4aaf7da269c40a1c5
ji4704 <ji4704@naver.com>
2024-05-23 17:29:14 +0900

    0523

---
+++ 0523/script.js
@@ -0,0 +1,64 @@
+function getName(a, b, c) {
+  const name = a;
+  const age = b;
+  const gender = c;
+  const value = name + " " + age + " " + gender;
+  return value;
+}
+const value = getName("Tom", 25, "Men");
+console.log(value);
+
+function isEven(input) {
+  const value = input % 2;
+  if (value > 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+console.log(isEven(3));
+console.log(isEven(4));
+
+function getDeaice(size) {
+  let deviceName;
+  if (size < 400) {
+    deviceName = "모바일";
+  } else if (size >= 400 && size < 800) {
+    deviceName = "태블릿";
+  } else if (size >= 800) {
+    deviceName = "피씨";
+  }
+  return deviceName;
+}
+console.log(getDeaice(399));
+console.log(getDeaice(1024));
+
+// const isPositive = (num) => {
+//     if (num > 0) {
+//         return true;
+//     }else {
+//         return false;
+//     }
+// };
+const isPositive = (num) => num > 0;
+console.log(isPositive(100));
+
+const getMaxNumber = (x, y) => {
+  if (x > y) {
+    return x;
+  } else {
+    return y;
+  }
+};
+console.log(getMaxNumber(5, 6));
+
+const 입장확인 = (나이) => {
+  const 제한나이 = 29;
+  if (나이 <= 제한나이) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(입장확인(25));
+console.log(입장확인(30));
